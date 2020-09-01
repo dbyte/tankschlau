76d294effe481da441241f121d5440792df97933
T. Fornalik <it-github@fornalik.de>
2020-09-01 23:00:36 +0200

    Added validation

--- src/main/java/de/fornalik/tankschlau/geo/Coordinates2D.java
+++ src/main/java/de/fornalik/tankschlau/geo/Coordinates2D.java
@@ -9,6 +9,7 @@ public class Coordinates2D {
   public Coordinates2D(double lat, double lon) {
     this.latitude = lat;
     this.longitude = lon;
+    throwOnInvalidCoordinates();
   }
 
   public double getLatitude() {
@@ -19,11 +20,25 @@ public class Coordinates2D {
     return longitude;
   }
 
+  private void throwOnInvalidCoordinates() throws InvalidCoordinatesException {
+    // Source: https://stackoverflow.com/a/47188298
+    if (latitude < -85.05112878 || latitude > 85.05112878
+        || longitude < -180.0 || longitude > 180.0)
+      throw new InvalidCoordinatesException(
+          "One ore mode coordinates are out of bounds. Lat: " + latitude + ", Lon: " + longitude);
+  }
+
   @Override
   public String toString() {
     return new StringJoiner(", ", Coordinates2D.class.getSimpleName() + "[", "]")
         .add("latitude=" + latitude)
         .add("longitude=" + longitude)
         .toString();
+  }
+
+  public static class InvalidCoordinatesException extends RuntimeException {
+    public InvalidCoordinatesException(String message) {
+      super(message);
+    }
   }
 }
