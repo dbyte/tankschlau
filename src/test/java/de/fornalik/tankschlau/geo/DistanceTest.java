e06a68baf0bf3d21c4f7d47527ca2dcb4acda474
T. Fornalik <it-github@fornalik.de>
2020-09-01 19:49:12 +0200

    Introduced parametrized tests

--- src/test/java/de/fornalik/tankschlau/geo/DistanceTest.java
+++ src/test/java/de/fornalik/tankschlau/geo/DistanceTest.java
@@ -1,44 +1,62 @@
 package de.fornalik.tankschlau.geo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DistanceTest {
 
   @Test
-  void getKm_shouldReturnValue() {
-    // Given
+  void constructor_shouldThrowOnNegativeKm() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> new Distance(-13.602)
+    );
+  }
+
+  @Test
+  void getKm_shouldReturnProperly() {
+    // given
     Distance sut = new Distance(40.2837456412);
 
-    // When
+    // when
     double actualKm = sut.getKm();
 
-    // Then
+    // then
     assertEquals(40.2837456412, actualKm);
   }
 
-  @Test
-  void setKm_shouldSetValue() {
-    // Given
+  @ParameterizedTest
+  @ValueSource(doubles = {120.001, 30.0, 0, 1876252.3938438743})
+  void setKm_shouldSetProperly(double validValue)
+      throws NoSuchFieldException, IllegalAccessException {
+    // given
     Distance sut = new Distance(0);
 
-    // When
-    sut.setKm(120.001);
+    // when
+    sut.setKm(validValue);
 
-    // Then
-    assertEquals(120.001,sut.getKm());
+    // then
+    final Field field = sut.getClass().getDeclaredField("km");
+    field.setAccessible(true);
+    assertEquals(validValue, field.get(sut));
   }
 
-  @Test
-  void setKm_shouldThrowOnNegativeValue() {
-    // Given
+  @ParameterizedTest
+  @ValueSource(doubles = {-0.04, -33, -234867762.342234219})
+  void setKm_shouldThrowOnNegativeValue(double negativeValue) {
+    // given
     Distance sut = new Distance(12.0);
 
-    // When Then
+    // when then
     assertThrows(
         IllegalArgumentException.class,
-        () -> sut.setKm(-0.4)
+        () -> sut.setKm(negativeValue)
     );
   }
 }
