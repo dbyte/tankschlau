993bda71ff58560a42f3b8e6cd768c2bf84b1432
T. Fornalik <it-github@fornalik.de>
2020-09-16 09:13:12 +0200

    Finished PetrolStationTest

--- src/test/java/de/fornalik/tankschlau/station/PetrolStationTest.java
+++ src/test/java/de/fornalik/tankschlau/station/PetrolStationTest.java
@@ -9,8 +9,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 class PetrolStationTest {
@@ -18,13 +17,17 @@ class PetrolStationTest {
   private static UUID uuidFixture;
   private static Address addressMock;
   private static Petrol petrolMock1, petrolMock2;
+  private static Set<Petrol> petrolsFixture;
 
   @BeforeAll
   static void beforeAll() {
-    PetrolStationTest.uuidFixture= UUID.fromString("fb48f03e-c16e-435d-a845-6e235612f88f");
+    PetrolStationTest.uuidFixture = UUID.fromString("fb48f03e-c16e-435d-a845-6e235612f88f");
     PetrolStationTest.addressMock = mock(Address.class);
     PetrolStationTest.petrolMock1 = mock(Petrol.class);
     PetrolStationTest.petrolMock2 = mock(Petrol.class);
+    PetrolStationTest.petrolsFixture = new HashSet<>();
+    petrolsFixture.add(petrolMock1);
+    petrolsFixture.add(petrolMock1);
   }
 
   @AfterAll
@@ -35,10 +38,82 @@ class PetrolStationTest {
     petrolMock2 = null;
   }
 
-  /*
-  Note: The constructor of the SUT is private, so we do not test it. Instead, tests can be
-  found at PetrolStationBuilderTest.
-   */
+  // region Constructor tests
+
+  @Test
+  void constructor_happy() {
+    // given, when
+    PetrolStation sut = new PetrolStation(
+        uuidFixture,
+        "brand",
+        true,
+        addressMock,
+        petrolsFixture
+    );
+
+    // then
+    assertEquals(uuidFixture, sut.uuid);
+    assertEquals("brand", sut.brand);
+    assertTrue(sut.isOpen);
+    assertEquals(addressMock, sut.address);
+    assertEquals(petrolsFixture, sut.getPetrols());
+  }
+
+  @Test
+  void constructor_throwsNullPointerOnGivenNullArgument() {
+    // given
+    Runnable callWithNullUUID = () -> new PetrolStation(
+        null,
+        "brand",
+        true,
+        addressMock,
+        petrolsFixture
+    );
+
+    Runnable callWithNullBrand = () -> new PetrolStation(
+        uuidFixture,
+        null,
+        true,
+        addressMock,
+        petrolsFixture
+    );
+
+    Runnable callWithNullAddress = () -> new PetrolStation(
+        uuidFixture,
+        "brand",
+        true,
+        null,
+        petrolsFixture
+    );
+
+    // when, then
+    assertThrows(NullPointerException.class,
+                 callWithNullUUID::run);
+
+    // when, then
+    assertThrows(NullPointerException.class,
+                 callWithNullBrand::run);
+
+    // when, then
+    assertThrows(NullPointerException.class,
+                 callWithNullAddress::run);
+  }
+
+  @Test
+  void constructor_doesNotThrowOnGivenNullPetrols() {
+    Runnable callWithNullPetrols = () -> new PetrolStation(
+        uuidFixture,
+        "brand",
+        true,
+        addressMock,
+        null
+    );
+
+    // when, then
+    assertDoesNotThrow(callWithNullPetrols::run);
+  }
+
+  // endregion
 
   @Test
   void getPetrols_happy() {
