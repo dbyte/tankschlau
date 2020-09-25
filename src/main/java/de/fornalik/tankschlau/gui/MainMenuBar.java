9435570d8e05ce4509240077dfc873459b6604b6
T. Fornalik <it-github@fornalik.de>
2020-09-25 13:29:19 +0200

    @SuppressWarnings for getMenuShortcutKeyMask as we're on target Java 8

--- src/main/java/de/fornalik/tankschlau/gui/MainMenuBar.java
+++ src/main/java/de/fornalik/tankschlau/gui/MainMenuBar.java
@@ -21,17 +21,18 @@ class MainMenuBar extends JMenuBar {
     JMenu fileMenu = new JMenu(TankSchlau.L10N.get("menu.File"));
     fileMenu.setMnemonic(KeyEvent.VK_F);
 
-    // region "Close window" menu item
+    // region "Close window" menu item - actually hides window.
 
-    JMenuItem itemClose = new JMenuItem(TankSchlau.L10N.get("menu.CloseWindow"));
+    JMenuItem itemHideWindow = new JMenuItem(TankSchlau.L10N.get("menu.CloseWindow"));
 
     // Shortcut cmd + W: Hides active focused window if present.
+    @SuppressWarnings("deprecation")
     KeyStroke strokeCmdW = KeyStroke.getKeyStroke(
         KeyEvent.VK_W,
         Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
-    itemClose.setAccelerator(strokeCmdW);
+    itemHideWindow.setAccelerator(strokeCmdW);
 
-    itemClose.addActionListener((event) -> {
+    itemHideWindow.addActionListener((event) -> {
       Optional<Window> window = Arrays
           .stream(Window.getWindows())
           .filter(Window::isFocused)
@@ -44,22 +45,23 @@ class MainMenuBar extends JMenuBar {
 
     // region "Show main window" menu item
 
-    JMenuItem itemNewMain = new JMenuItem(TankSchlau.L10N.get("menu.ShowMainWindow"));
+    JMenuItem itemShowMainWindow = new JMenuItem(TankSchlau.L10N.get("menu.ShowMainWindow"));
 
     // Shortcut cmd + N: Shows main window if it's not visible
+    @SuppressWarnings("deprecation")
     KeyStroke strokeCmdN = KeyStroke.getKeyStroke(
         KeyEvent.VK_N,
         Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
-    itemNewMain.setAccelerator(strokeCmdN);
-    itemNewMain.addActionListener((event) -> {
+    itemShowMainWindow.setAccelerator(strokeCmdN);
+    itemShowMainWindow.addActionListener((event) -> {
       if (!mainFrame.isVisible())
         mainFrame.setVisible(true);
     });
 
     // endregion
 
-    fileMenu.add(itemClose);
-    fileMenu.add(itemNewMain);
+    fileMenu.add(itemHideWindow);
+    fileMenu.add(itemShowMainWindow);
 
     this.add(fileMenu);
   }
