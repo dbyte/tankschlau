63f35b0bb9cfbb62fad6bf26c53206817ee6398f
T. Fornalik <it-github@fornalik.de>
2020-09-29 14:38:30 +0200

    Added some tests

--- src/test/java/de/fornalik/tankschlau/util/UserPreferencesTest.java
+++ src/test/java/de/fornalik/tankschlau/util/UserPreferencesTest.java
@@ -2,14 +2,24 @@ package de.fornalik.tankschlau.util;
 
 import de.fornalik.tankschlau.geo.Address;
 import de.fornalik.tankschlau.geo.Geo;
+import de.fornalik.tankschlau.station.PetrolType;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.Mockito;
 
 import java.util.Optional;
+import java.util.prefs.BackingStoreException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Tests for UserPreferences. We execute combined write/read tests.
+ */
 class UserPreferencesTest {
   private static Address addressMock;
   private static Geo geoMock;
@@ -39,28 +49,51 @@ class UserPreferencesTest {
   }
 
   @BeforeEach
-  void setUp() {
-    prefs = new UserPreferences(this.getClass());
+  void setUp() throws BackingStoreException {
+    Class<?> thisClazz = this.getClass();
+
+    // Clear the node of possible persisted preferences ...
+    new UserPreferences(thisClazz).getJavaPref().removeNode();
+
+    // ... and start with a fresh one.
+    prefs = new UserPreferences(thisClazz);
   }
 
   @Test
-    // TODO
-  void setUserAddress_storesDataProperly() {
-    // given
-
+  void writeUserAddress_writesProperly() {
     // when
-    prefs.setUserAddress(addressMock);
+    prefs.writeUserAddress(addressMock);
+    Address actualAddress = prefs.readUserAddress();
 
     // then
+    assertEquals(addressMock.getName(), actualAddress.getName());
+    assertEquals(addressMock.getCity(), actualAddress.getCity());
+    assertEquals(addressMock.getPostCode(), actualAddress.getPostCode());
+    assertEquals(addressMock.getStreet(), actualAddress.getStreet());
+    assertEquals(addressMock.getHouseNumber(), actualAddress.getHouseNumber());
+
+    assertTrue(actualAddress.getGeo().isPresent());
+    Geo actualGeo = actualAddress.getGeo().get();
+    assert addressMock.getGeo().isPresent();
+
+    assertEquals(addressMock.getGeo().get().getLatitude(), actualGeo.getLatitude());
+    assertEquals(addressMock.getGeo().get().getLongitude(), actualGeo.getLongitude());
+    assertEquals(addressMock.getGeo().get().getDistance(), actualGeo.getDistance());
   }
 
   @Test
     // TODO
-  void setUserGeo() {
+  void writeUserGeo() {
   }
 
-  @Test
-    // TODO
-  void setPreferredPetrolType() {
+  @ParameterizedTest
+  @EnumSource(PetrolType.class)
+  void writePreferredPetrolType_writesProperly(PetrolType givenPetrolType) {
+    // when
+    prefs.writePreferredPetrolType(givenPetrolType);
+    PetrolType actualPetrolType = prefs.readPreferredPetrolType();
+
+    // then
+    assertEquals(givenPetrolType, actualPetrolType);
   }
 }
