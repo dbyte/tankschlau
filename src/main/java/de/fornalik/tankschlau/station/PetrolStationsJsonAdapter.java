06f15555c60054def08e016dc6a21039f0b1f4c4
T. Fornalik <it-github@fornalik.de>
2020-10-01 22:28:11 +0200

    Added copyright

--- src/main/java/de/fornalik/tankschlau/station/PetrolStationsJsonAdapter.java
+++ src/main/java/de/fornalik/tankschlau/station/PetrolStationsJsonAdapter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 Tammo Fornalik
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.fornalik.tankschlau.station;
 
 import com.google.gson.*;
@@ -88,20 +104,20 @@ public class PetrolStationsJsonAdapter extends TypeAdapter<List<PetrolStation>> 
     // 2. Build a new petrol station, by filling in previously created data and handling
     // custom types.
     return PetrolStationBuilder.create(adaptUUID(station))
-        .withBrand(partialPetrolStation.brand)
-        .withIsOpen(partialPetrolStation.isOpen)
-        .withPetrols(Petrols.createFromJson(station))
-        .withAddress(Address.createFromJson(station)) //throws
-        .build();
+                               .withBrand(partialPetrolStation.brand)
+                               .withIsOpen(partialPetrolStation.isOpen)
+                               .withPetrols(Petrols.createFromJson(station))
+                               .withAddress(Address.createFromJson(station)) //throws
+                               .build();
   }
 
   private UUID adaptUUID(JsonObject station) throws MissingElementException {
     String property = "id";
 
     return Optional.ofNullable(station.get(property))
-        .map(JsonElement::getAsString)
-        .map(UUID::fromString)
-        .orElseThrow(() -> new MissingElementException(property));
+                   .map(JsonElement::getAsString)
+                   .map(UUID::fromString)
+                   .orElseThrow(() -> new MissingElementException(property));
   }
 
   /**
