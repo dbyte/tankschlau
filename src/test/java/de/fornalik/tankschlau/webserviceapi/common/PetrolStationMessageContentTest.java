b7725d590204ada54a4ea0ed6199860e1d7aa489
T. Fornalik <it-github@fornalik.de>
2020-10-15 12:59:42 +0200

    Applied 1st unit test for PetrolStationMessageContentTest

--- src/test/java/de/fornalik/tankschlau/webserviceapi/common/PetrolStationMessageContentTest.java
+++ src/test/java/de/fornalik/tankschlau/webserviceapi/common/PetrolStationMessageContentTest.java
@@ -16,49 +16,108 @@
 
 package de.fornalik.tankschlau.webserviceapi.common;
 
-import de.fornalik.tankschlau.geo.Address;
 import de.fornalik.tankschlau.geo.Geo;
+import de.fornalik.tankschlau.station.Petrol;
 import de.fornalik.tankschlau.station.PetrolStation;
 import de.fornalik.tankschlau.station.PetrolType;
-import de.fornalik.tankschlau.station.testhelp.PetrolStationTestHelper;
 import de.fornalik.tankschlau.util.Localization;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 class PetrolStationMessageContentTest {
-  PetrolStationTestHelper domainHelp;
-  private PetrolStationMessageContent messageContent; // SUT
+  private PetrolStationMessageContent messageContentStub; // SUT
   private Localization l10nMock;
   private PetrolStation petrolStationMock;
-  private PetrolType petrolTypeE5;
-  private Address addressMock;
+  private Petrol petrolMock;
+  // private Address addressMock;
   private Geo geoMock;
 
   @BeforeEach
   void setUp() {
-    domainHelp = new PetrolStationTestHelper();
     l10nMock = mock(Localization.class);
     petrolStationMock = mock(PetrolStation.class, Mockito.RETURNS_DEEP_STUBS);
-    petrolTypeE5 = PetrolType.E5;
-    addressMock = mock(Address.class);
+    petrolMock = mock(Petrol.class);
+    // addressMock = mock(Address.class);
     geoMock = mock(Geo.class);
 
-    messageContent = mock(PetrolStationMessageContent.class, Mockito.CALLS_REAL_METHODS);
+    messageContentStub = new PetrolStationMessageContentStub(l10nMock);
   }
 
   @Test
-  void setMessage_() {
+  void setMessage_formatsTextAsExpected() {
     // given
-    when(petrolStationMock.getAddress().getName()).thenReturn("Some station name");
+    String expectedBestPriceString = "Neuer Bestpreis! DIESEL: 1,219 €";
+    String expectedDistanceString = "5.3 km entfernt";
+    String expectedPetrolStationName = "Tankstelle Gluckenkopp";
+    String expectedStreetName = "Schlampstrasse 55";
+    String expectedOpenedString = "jetzt geöffnet";
+    String expectedClosedString = "jetzt geschlossen";
+
+    when(geoMock.getDistanceAwayString(l10nMock)).thenReturn(expectedDistanceString);
+
+    when(petrolStationMock.isOpen()).thenReturn(true);
+    when(petrolStationMock.getAddress().getName()).thenReturn(expectedPetrolStationName);
+    when(petrolStationMock.getAddress().getGeo()).thenReturn(Optional.of(geoMock));
+    when(petrolStationMock.getAddress().getStreetAndHouseNumber()).thenReturn(expectedStreetName);
+    when(petrolStationMock.findPetrol(any())).thenReturn(Optional.ofNullable(petrolMock));
+
+    when(l10nMock.get("msg.BestPrice", petrolMock.getTypeAndPrice()))
+        .thenReturn(expectedBestPriceString);
+
+    when(l10nMock.get("msg.NowOpen")).thenReturn(expectedOpenedString);
+    when(l10nMock.get("msg.NowClosed")).thenReturn(expectedClosedString);
+    when(l10nMock.get("msg.NoPetrolDataForStation"))
+        .thenReturn("Keine Spritpreise für diese Station vorhanden.");
 
     // when
-    messageContent.setMessage(petrolStationMock, petrolTypeE5);
+    messageContentStub.setMessage(petrolStationMock, PetrolType.E5);
 
     // then
+    String expectedConcatenationResult = expectedBestPriceString
+        + "\n" + expectedDistanceString
+        + "\n\n" + expectedPetrolStationName + " - " + expectedOpenedString
+        + "\n" + expectedStreetName;
 
+    assertEquals(expectedConcatenationResult, messageContentStub.getMessage());
+  }
+
+  // Stub that implements the abstract subject under test.
+  private static class PetrolStationMessageContentStub extends PetrolStationMessageContent {
+    private String title;
+    private String message;
+
+    protected PetrolStationMessageContentStub(Localization mock) {
+      super(mock);
+    }
+
+    @Override
+    public String getTitle() {
+      return this.title;
+    }
+
+    @Override
+    public void setTitle(String title) {
+      this.title = title;
+    }
+
+    @Override
+    public String getMessage() {
+      return this.message;
+    }
+
+    @Override
+    public void setMessage(String text) {
+      this.message = text;
+    }
+
+    @Override
+    public void reset() {
+    }
   }
 }
