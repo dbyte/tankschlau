6aeaec336d4feb5d330106e730423f1ff3df14ff
T. Fornalik <it-github@fornalik.de>
2020-10-16 01:25:47 +0200

    Finished tests for priceFormat(), kmFormat()

--- src/test/java/de/fornalik/tankschlau/util/LocalizationTest.java
+++ src/test/java/de/fornalik/tankschlau/util/LocalizationTest.java
@@ -3,6 +3,8 @@ package de.fornalik.tankschlau.util;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -85,5 +87,74 @@ class LocalizationTest {
     assertEquals(
         "*** Localized string not found for key 'DoesNotExist' ***",
         actualLocalizedText);
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {
+      "2,898:2.89756",
+      "2,00:2",
+      "1,777:1.777",
+  }, delimiter = ':')
+  void priceFormat_HasMax3AndMin2Fractions(String expectedPriceString, double givenPrice) {
+    // given
+    String actualPriceString;
+
+    // when
+    actualPriceString = sutGerman.priceFormat().format(givenPrice);
+
+    // then
+    assertEquals(expectedPriceString, actualPriceString);
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {
+      "2.00:2.00",
+      "1.219:1.219"
+  }, delimiter = ':')
+  void priceFormat_setsExpectedDecimalSeparatorForLocale(
+      String expectedPriceString,
+      double givenPrice) {
+    // given
+    String actualPriceString;
+
+    // when
+    actualPriceString = sutEnglish.priceFormat().format(givenPrice);
+
+    // then
+    assertEquals(expectedPriceString, actualPriceString);
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {
+      "2,9:2.89756",
+      "2:2.00",
+      "1,5:1.5",
+      "1,34:1.34",
+  }, delimiter = ':')
+  void kmFormat_HasMax2AndMin0Fractions(String expectedKmString, double givenKm) {
+    // given
+    String actualKmString;
+
+    // when
+    actualKmString = sutGerman.kmFormat().format(givenKm);
+
+    // then
+    assertEquals(expectedKmString, actualKmString);
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {
+      "2.00:2.00",
+      "1.219:1.219"
+  }, delimiter = ':')
+  void kmFormat_setsExpectedDecimalSeparatorForLocale(String expectedKmString, double givenKm) {
+    // given
+    String actualKmString;
+
+    // when
+    actualKmString = sutEnglish.priceFormat().format(givenKm);
+
+    // then
+    assertEquals(expectedKmString, actualKmString);
   }
 }
