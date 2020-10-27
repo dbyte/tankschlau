3c6388e57917b0e68fbf4c61fadd3d533080d331
T. Fornalik <it-github@fornalik.de>
2020-10-27 08:08:18 +0100

    Cleanup

--- src/main/java/de/fornalik/tankschlau/storage/PetrolStationsWorker.java
+++ src/main/java/de/fornalik/tankschlau/storage/PetrolStationsWorker.java
@@ -16,10 +16,9 @@
 
 package de.fornalik.tankschlau.storage;
 
-import de.fornalik.tankschlau.geo.Address;
 import de.fornalik.tankschlau.geo.Geo;
 import de.fornalik.tankschlau.station.PetrolStation;
-import de.fornalik.tankschlau.station.PetrolStationBuilder;
+import de.fornalik.tankschlau.util.Localization;
 import de.fornalik.tankschlau.util.RunnableCallbackWorker;
 
 import java.util.*;
@@ -31,8 +30,9 @@ import java.util.logging.Logger;
  */
 public class PetrolStationsWorker implements RunnableCallbackWorker<List<PetrolStation>> {
   public static final Logger LOGGER = Logger.getLogger(PetrolStationsWorker.class.getName());
-  private final PetrolStationsService petrolStationsService;
+  private static final Localization L10N = Localization.getInstance();
 
+  private final PetrolStationsService petrolStationsService;
   private Geo userGeo;
   private Consumer<List<PetrolStation>> callback;
 
@@ -85,57 +85,12 @@ public class PetrolStationsWorker implements RunnableCallbackWorker<List<PetrolS
     List<PetrolStation> data = petrolStationsService.getNeighbourhoodStations((userGeo));
     Optional<String> errorMessage = petrolStationsService.getTransactInfo().getErrorMessage();
 
-    /*if (errorMessage.isPresent())
-      LOGGER.warning(l10n.get("msg.ErrorServerConnection", errorMessage));
+    if (errorMessage.isPresent())
+      LOGGER.warning(L10N.get("msg.ErrorServerConnection", errorMessage));
 
     if (data.size() == 0) {
-      LOGGER.warning(l10n.get("msg.NoPetrolStationsFoundInNeighbourhood"));
-    }*/
-
-    return data;
-  }
-
-  private List<PetrolStation> createDemoData() {
-    List<PetrolStation> data = new ArrayList<>();
-
-    Geo geo = new Geo(52.019283673, 48.1625243, 4.3);
-    Address address = new Address(
-        "ARAL am Westend",
-        "Büllibällistraße",
-        "41",
-        "München",
-        "80000",
-        geo);
-    PetrolStation station = PetrolStationBuilder.create(UUID.randomUUID())
-        .withAddress(address)
-        .withBrand("ARAL")
-        .withIsOpen(true)
-        .build();
-    data.add(station);
-
-    geo = new Geo(51.0191031, 49.1625243, 5.2);
-    address = new Address(
-        "ESSO Burgwall, Inh. Izmir Schlecht",
-        "Burgwall",
-        "113",
-        "München",
-        "80001",
-        geo);
-    station = PetrolStationBuilder.create(UUID.randomUUID())
-        .withAddress(address)
-        .withBrand("ESSO")
-        .withIsOpen(false)
-        .build();
-    data.add(station);
-
-    geo = new Geo(50.139461, 47.937462, 5.0);
-    address = new Address("OMV Schlamp", "Dödelberghain", "12b", "München", "80002", geo);
-    station = PetrolStationBuilder.create(UUID.randomUUID())
-        .withAddress(address)
-        .withBrand("OMV")
-        .withIsOpen(false)
-        .build();
-    data.add(station);
+      LOGGER.warning(L10N.get("msg.NoPetrolStationsFoundInNeighbourhood"));
+    }
 
     return data;
   }
